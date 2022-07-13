138965e91371ae3b0e9d491035744e64b4d03abf
RFdeGroot <roelofdegroot@icloud.com>
2022-07-13 18:26:50 +0200

    check

--- 1. Build a Passenger Counter App/4. Write your first JavaScript variable/index.js
+++ 1. Build a Passenger Counter App/4. Write your first JavaScript variable/index.js
@@ -4,7 +4,9 @@ let count = 0
 
 console.log(count)
 
+let myAge = 42
 
+console.log(myAge)
 // 1. Create a variable, myAge, and set its value to your age
 
 // 2. Log the myAge variable to the console
--- 1. Build a Passenger Counter App/7. Adding a button/index.js
+++ 1. Build a Passenger Counter App/7. Adding a button/index.js
@@ -4,21 +4,12 @@
 // count = count + 1
 // console.log(count)
 
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
+//Create increment function for button
 
-let bonusPoints = 50
-console.log(bonusPoints)
+function increment() {
+    console.log("The button was clicked")
 
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+}
 
 
 
